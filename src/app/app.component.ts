e4a8f6d0c4520f217594770fc6d087ef120c365d
Andres Felipe Torres <andres.torres@ceiba.com.co>
2019-09-22 21:05:42 -0500

    Video clase 36

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Nueva APP';
-  title2 = "nuevo";
+  Nombre = 'Nueva APP';
+  Apellido = "nuevo";
 }
